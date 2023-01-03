956b304881eb38bbc1804f75e7ab3d4016aeb4c8
Simon Walker <s.r.walker101@googlemail.com>
2023-01-03 11:46:09 +0000

    include nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Flake utils demo";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.hugo
+          ];
+        };
+      }
+    );
+}
